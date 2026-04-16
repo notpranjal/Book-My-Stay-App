f5dd1f4a38d7ff4baf15faaacc286054af3fd341
Pranjal Jaiswal <114137463+notpranjal@users.noreply.github.com>
2026-04-16 22:23:10 +0530

    Create BookMyStayApp.java

---
+++ src/feature/src/BookMyStayApp.java
@@ -0,0 +1,15 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        String applicationName = "BookMyStay Hotel Booking System";
+        String version = "v1.0";
+
+        System.out.println("======================================");
+        System.out.println(" Welcome to " + applicationName);
+        System.out.println(" Version: " + version);
+        System.out.println(" Application started successfully.");
+        System.out.println("======================================");
+
+    }
+}
